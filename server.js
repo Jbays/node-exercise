45bd2def0d8c488650747cd30dcf8c884d16cf6a
jbays <jjbaize@gmail.com>
2018-11-13 08:04:37 -0600

    POST and GET request working.  POST route correctly increments active_visitors.  GET route correctly returns total number of active_visitors.

--- server.js
+++ server.js
@@ -6,7 +6,23 @@ const app = express();
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
+let activeVisitors = 0;
+
+app.post('/metrics/active_visitors',(req,res)=>{
+  if ( typeof req.body.value === 'number' ) {
+    activeVisitors += req.body.value;
+    return res.sendStatus(200)
+  } else {
+    return res.status(400).send('request body must be data type number!');
+  }
+})
+
+app.get('/metrics/active_visitors/sum',(req,res)=>{
+  return res.status(200).send(`This past hour your site's had: ${activeVisitors} visitors`);
+})
+
+// setInterval
+
 app.listen(PORT,()=>{
   console.log(`Listening on port:${PORT}`);
 })
-
